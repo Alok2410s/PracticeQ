03a71518769652e5abef419d2b7fd5837d76606c
Alok kumar <ak0144709@gmail.com>
2026-06-04 14:33:14 +0530

    feat: add Alok class with main method to print hello world and sum

---
+++ First.java
@@ -0,0 +1,8 @@
+class Alok{
+      public static void main(String args[]){
+         System.out.println("hello world");
+            int a=10,b=23;
+             int c=b-a;
+          System.out.println("sum is" +c);
+}
+}
