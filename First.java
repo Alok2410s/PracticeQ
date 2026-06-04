35bb283ed824f4aa840726b9c271910e68b43b48
Alok kumar <ak0144709@gmail.com>
2026-06-04 20:03:42 +0530

    refactor: rename class from Alok to First and fix output formatting

--- First.java
+++ First.java
@@ -1,8 +1,8 @@
-class Alok{
+class First{
       public static void main(String args[]){
          System.out.println("hello world");
             int a=10,b=23;
              int c=b-a;
-          System.out.println("sum is" +c);
+         System.out.println("sum is: " + c);
 }
 }
