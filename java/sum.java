609f02abcbcbdb0e84892daea517aae6c7b30d5c
Alok kumar <ak0144709@gmail.com>
2026-06-08 14:29:04 +0530

    fix: improve readability and formatting in sum class

--- java/sum.java
+++ java/sum.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class sum {
     public static void main(String[] args){
-        java.util.Scanner sc=new java.util.Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 number:");
         int a=sc.nextInt(), b=sc.nextInt();
         int c=a+b;
-        System.out.println("Sum is"+c);
+        System.out.println("Sum is: "+c);
+        sc.close();
     }
 }
