b4ce06b6dc7694b7f857b4ec0e251203843f3a98
Alok kumar <ak0144709@gmail.com>
2026-06-08 14:24:16 +0530

    feat: add sum class with main method for calculating the sum of two integers and update launch configuration

---
+++ java/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args){
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+        int a=sc.nextInt(), b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Sum is"+c);
+    }
+}
